d30c1e2dfa5eab0b89301fbabb76381968589797
cristiano <cristiano.lourenco.ao@gmail.com>
2023-08-08 20:51:40 +0100

    spliting the code

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,19 +6,32 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: <Color>[
-              Color(0xFF652FC2),
-              Color(0xFF2A046B),
-            ],
-          ),
+      body: GradientContainer(),
+    );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: <Color>[
+            Color(0xFF652FC2),
+            Color(0xFF2A046B),
+          ],
         ),
-        child: const Center(
-          child: Text('Hello World!'),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
         ),
       ),
     );
