6ba471b6e34abe09967e994357e039b43bda1fb5
cristiano <cristiano.lourenco.ao@gmail.com>
2023-08-09 13:42:01 +0100

    using statefull widgets

---
+++ lib/dice_roller.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+
+  // Function to roll dice
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          scale: 3,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 // import 'package:dice_app_intro/styled_text.dart';
+import 'package:dice_app_intro/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -11,15 +12,13 @@ class GradientContainer extends StatelessWidget {
     super.key,
   });
 
-  final Color color1, color2;
-
   // Multiple constructors
   const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
-  // Function to roll dice
-  void rollDice() {}
+  // VARIABLES
+  final Color color1, color2;
 
   @override
   Widget build(context) {
@@ -31,27 +30,8 @@ class GradientContainer extends StatelessWidget {
           colors: <Color>[color1, color2],
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/dice-2.png',
-              // width: 200,
-              // I choose to use scale instead
-              scale: 3,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
